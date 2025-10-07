d36e0ca05175b75d68d8feac4dc10e9488be449d
swaraj27 <swarajpawar27@gmail.com>
2025-10-07 22:43:18 +0530

    two sum leet code

--- src/leetcode/TwoSums.java
+++ src/leetcode/TwoSums.java
@@ -1,9 +1,40 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSums {
+
+
+    public  static int[] twoSum(int [] nums, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i< nums.length; i++){
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement), i};
+            }
+
+            map.put(nums[i],i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
 
     public static void main(String[] args) {
 
+       /* Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]*/
+        int nums[] = {2, 7, 11, 15};
+        int target = 9;
+
+        int result [] = twoSum(nums,target);
+
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
 
     }
+
+
 }
