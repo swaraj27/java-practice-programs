143f3ae50f58f05d689e5764abd7e91474991a9e
swaraj27 <swarajpawar27@gmail.com>
2025-11-10 00:02:38 +0530

    length of last word

---
+++ src/leetcode/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String str = "   fly me   to   the moon  ";
+        System.out.println(findLengthOfLastWord(str));
+
+    }
+
+    public static int findLengthOfLastWord(String str){
+
+        String strArr[] = str.trim().split("\\s+");
+        int last  = strArr.length - 1;
+        return strArr[last].length();
+    }
+}
--- src/leetcode/RemoveDuplicates.java
+++ src/leetcode/RemoveDuplicates.java
@@ -13,5 +13,23 @@ public class RemoveDuplicates {
                 .toArray();
 
         System.out.println(Arrays.toString(uniqueArr));
+
+        System.out.println(removeDuplicates(arr));
+    }
+
+    public static int removeDuplicates(int arr[]){
+        Arrays.sort(arr); // {1,1,2,2,3}
+
+        int i=0;
+        for(int j=1; j < arr.length; j++){
+
+            if(arr[j] != arr[i]){
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+
+        return i + 1;
+
     }
 }
